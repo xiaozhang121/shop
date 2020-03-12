09120027f46c337ec06809c1820eedfb0c96c29d
上下文 <1031247267@qq.com>
2020-03-12 16:00:14 +0800

    获取用户授权信息

--- src/pages/auth/index.vue
+++ src/pages/auth/index.vue
@@ -1,12 +1,28 @@
 <template>
   <view>
-    授权登录
+    <button open-type='getUserInfo' @getuserinfo='getuserinfo' type="primary">点击授权</button>
   </view>
 </template>
 <script>
 export default {
   data () {
     return {}
+  },
+  methods: {
+    getuserinfo (e) {
+      // 1、获取微信的授权信息
+      const {
+        encryptedData,
+        iv,
+        rawData,
+        signature
+      } = e.detail
+      //2、获取code
+
+      //3、调用后台接口获取token信息
+
+      //4、获取token后，进行缓存，然后跳回到上一个页面
+    }
   }
 }
 </script>
