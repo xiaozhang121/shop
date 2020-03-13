18f6cbdb4dbe627e8f5799626f31af1f6fdddac3
上下文 <1031247267@qq.com>
2020-03-13 09:54:30 +0800

    跳转到订单确认页面

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -140,6 +140,9 @@
         })
         // 获取订单号，跳转到订单确认页面
         const { order_number } = message
+        uni.navigateTo({
+          url: '/pages/order/index?order_number=' + order_number
+        })
       },
       getAddress () {
         // 获取收货地址
